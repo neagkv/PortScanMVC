dcb0622e18a90d158160f55aa9ce6e1644523acb
neagkv <neagkv@gmail.com>
2018-06-30 06:17:05 -0400

    new project

---
+++ src/main/java/com/Dev/PortScanMVC/PortScanMvcApplication.java
@@ -0,0 +1,12 @@
+package com.Dev.PortScanMVC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortScanMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortScanMvcApplication.class, args);
+	}
+}
